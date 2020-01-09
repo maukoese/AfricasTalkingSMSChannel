1a415cd5ec6474c460db945c76481a8d48b6f648
Mauko Maunde <maukoese@gmail.com>
2020-01-09 07:27:22 -0500

    Update AfricasTalkingSMSChannel.php

--- AfricasTalkingSMSChannel.php
+++ AfricasTalkingSMSChannel.php
@@ -3,6 +3,7 @@
 namespace App\Channels;
 
 use Illuminate\Notifications\Notification;
+use AfricasTalking\SDK\AfricasTalking;
 
 class AfricasTalkingSMSChannel
 {
@@ -18,5 +19,52 @@ class AfricasTalkingSMSChannel
         $message = $notification->toText($notifiable);
 
         // Send notification to the $notifiable instance...
+        $username = env('AT_USERNAME', 'sandbox');
+        $apiKey   = env('AT_PASSWORD', 'sandbox');
+        $AT       = new AfricasTalking($username, $apiKey);
+
+        $sms      = $AT->sms();
+        try {
+            $response = $this->sms->send([
+                'to'      => $notification->to,
+                'message' => $message
+            ]);
+
+            $codes = [
+                100 => "Processed",
+                101 => "Sent",
+                102 => "Queued",
+                401 => "RiskHold",
+                402 => "InvalidSenderId",
+                403 => "InvalidPhoneNumber",
+                404 => "UnsupportedNumberType",
+                405 => "InsufficientBalance",
+                406 => "UserInBlacklist",
+                407 => "CouldNotRoute",
+                500 => "InternalServerError",
+                501 => "GatewayError",
+                502 => "RejectedByGateway"
+            ];
+
+            // {
+            //     "SMSMessageData": {
+            //         "Message": "Sent to 1/1 Total Cost: KES 0.8000",
+            //         "Recipients": [{
+            //             "statusCode": 101,
+            //             "number": "+254711XXXYYY",
+            //             "status": "Success",
+            //             "cost": "KES 0.8000",
+            //             "messageId": "ATPid_SampleTxnId123"
+            //         }]
+            //     }
+            // }
+
+
+            if (!isset($response["SMSMessageData"])) {
+                throw CouldNotSendNotification::serviceRespondedWithAnError($th->getMessage());
+            }
+        } catch (\Throwable $th) {
+            throw CouldNotSendNotification::serviceRespondedWithAnError($th->getMessage());
+        }
     }
 }
