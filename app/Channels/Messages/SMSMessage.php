6b0133ea6e417cbc6177669622cac1501cc469e0
Mauko Maunde <maukoese@gmail.com>
2020-01-09 07:31:36 -0500

    Create SMSMessage.php

---
+++ app/Channels/Messages/SMSMessage.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Channels\Messages;
+
+class SMSMessage
+{
+  public $content;
+  
+  public function content($content)
+  {
+    $this->content = $content;
+
+    return $this;
+  }
+}
